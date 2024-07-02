52ed7b903ecd8b3682de703eb0aee5f1767a971d
audrey-tan <64370549+audrey-tan@users.noreply.github.com>
2024-07-02 09:45:15 +0700

    initial commit

---
+++ jaundice/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+
+import base64
+import uuid
+from django.core.files.base import ContentFile
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from datetime import datetime
+from jaundice_api.models import Newborn, ImageModel
+
+# Create your views here.
+
+
+@api_view(['POST'])
+def saveData(request):
+    data = request.data
+
+    try: 
+        dateOfBirth = data.get('dateOfBirth')
+        timeOfBirth = data.get('timeOfBirth')
+        birthWeight = data.get('birthWeight')
+        gender = data.get('gender')
+        fSkinTone = data.get('fSkinTone')
+        mSkinTone = data.get('mSkinTone')
+        kramer = data.get('kramer')
+        jaundice = data.get('jaundice')
+        bilirubin = data.get('bilirubin')
+        foreheads = data.get('foreheads')
+
+        dateOfBirth = datetime.strptime(dateOfBirth, '%Y-%m-%d').date()
+        timeOfBirth = datetime.strptime(timeOfBirth, '%H:%M').time()
+
+        newborn = Newborn.objects.create(
+            dateOfBirth=dateOfBirth,
+            timeOfBirth=timeOfBirth,
+            birthWeight=birthWeight,
+            gender=gender,
+            fSkinTone=fSkinTone,
+            mSkinTone=mSkinTone,
+            kramer=kramer,
+            jaundice=jaundice,
+            bilirubin=bilirubin
+        )
+
+        for f in foreheads:
+            img_data = base64.b64decode(f.encode())
+
+            # Save image to model
+            image = ImageModel.objects.create()
+            image.img.save(f"{uuid.uuid4()}.jpg", ContentFile(img_data))
+            newborn.foreheads.add(image)
+
+        newborn.save()
+        return Response("Successfully saved.", status=status.HTTP_201_CREATED)
+
+    except Exception as err:
+        return Response({"error": str(err)}, status=status.HTTP_400_BAD_REQUEST)
+
---
+++ jaundice_api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ImageModel(models.Model):
+    img = models.ImageField()
+
+class Newborn(models.Model):
+    dateOfBirth = models.DateField()
+    timeOfBirth = models.TimeField()
+    birthWeight = models.PositiveIntegerField()
+    gender = models.IntegerField()
+    fSkinTone = models.IntegerField()
+    mSkinTone = models.IntegerField()
+    kramer = models.IntegerField()
+    jaundice = models.BooleanField()
+    bilirubin = models.FloatField()
+    foreheads = models.ManyToManyField(ImageModel)
+
+
+
+
+
+
